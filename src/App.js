6c5274b3c88836eaeb24fa52a28de1e559c3c1c6
saivamsi1911 <sv191000@gmail.com>
2021-03-08 17:11:22 +0530

    github api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import axios from 'axios';
+import Search from './components/users/Search';
 
 class App extends Component {
+	state = {
+		users: [],
+		loading: false
+	};
+	// async componentDidMount() {
+	// 	this.setState({ loading: true });
+
+	// 	const res = await axios.get(
+	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+	// 	);
+
+	// 	this.setState({ loading: false, users: res.data });
+	// }
+
+	searchUsers = async (text) => {
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		console.log(text);
+		this.setState({ loading: false, users: res.data.items });
+	};
 	render() {
 		return (
 			<div className='App'>
 				<Navbar title='Github Finder' icon='fab fa-github' />
-				<UserItem />
+				<Search searchUsers={this.searchUsers} />
+				<Users loading={this.state.loading} users={this.state.users} />
 			</div>
 		);
 	}
